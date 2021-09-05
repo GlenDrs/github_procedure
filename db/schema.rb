12937691d2d1f1a280f911a821cfc214e7ae3c7e
John78468 <73955607+John78468@users.noreply.github.com>
2021-09-05 18:51:41 +0200

    Feature : admin (#2)
    * Create model add_admin and migration
    
    * Create controller Admins and wiew index
    
    * Fix index wording for admins
    
    Co-authored-by: john <https://github.com/John78468/dotenv_test.git>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_05_155725) do
+ActiveRecord::Schema.define(version: 2021_09_05_155850) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.text "adress"
+    t.string "zip_code"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
